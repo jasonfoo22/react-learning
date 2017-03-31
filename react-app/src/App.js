bb2acd690c36ebdbaf785960577ff6afd479c7d4
Jason Foo <jason.foo@seekasia.com>
2017-03-31 18:01:29 +0800

    First commit and some learning

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <span>sdsadsa</span>
+            </div>
+        )
+    }
+}
+
+// const App = () => <h1>hey</h1>
+
+export default App
